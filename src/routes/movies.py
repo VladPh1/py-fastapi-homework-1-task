baa0dde62380cab9c6602c440953d0e029136f09
Vladyslav Pihol <freepgvlad@gmail.com>
2025-11-30 19:57:04 +0200

    Solution1

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -10,28 +10,32 @@ router = APIRouter()
 
 
 @router.get("/movies/", response_model=MovieListResponseSchema)
-async def get_movies(page: int = Query(1, ge=1), per_page: int = Query(10, ge=1, le=20), db: AsyncSession = Depends(get_db)):
+async def get_movies(
+        page: int = Query(1, ge=1),
+        per_page: int = Query(10, ge=1, le=20),
+        db: AsyncSession = Depends(get_db)
+):
     result = await db.execute(select(MovieModel).offset((page - 1) * per_page).limit(per_page))
     movies = result.scalars().all()
     count_query = select(func.count()).select_from(MovieModel)
     total_items = (await db.execute(count_query)).scalar_one()
     total_pages = (total_items + per_page - 1) // per_page
+
     if page == 1:
         prev_page = None
     else:
-        prev_page = f"/theater/movies/?page={page-1}&per_page={per_page}"
+        prev_page = f"/theater/movies/?page={page - 1}&per_page={per_page}"
     if page >= total_pages:
         next_page = None
     else:
-        next_page = f"/theater/movies/?page={page+1}&per_page={per_page}"
+        next_page = f"/theater/movies/?page={page + 1}&per_page={per_page}"
     if total_items == 0:
         raise HTTPException(status_code=404, detail="No movies found.")
     return {"movies": movies,
             "prev_page": prev_page,
             "next_page": next_page,
             "total_pages": total_pages,
-            "total_items": total_items
-    }
+            "total_items": total_items}
 
 
 @router.get("/movies/{movie_id}/", response_model=MovieDetailResponseSchema)
